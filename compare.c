aa08bd60d7fa6c30455b460d39745930b08425ef
Hao Nguyen <hao@haonguyen.tech>
2024-01-14 16:20:27 +0700

    feat: add compare

---
+++ compare.c
@@ -0,0 +1,16 @@
+// Copyright (c) 2024 Hao Nguyen. All Rights Reserved.
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  int x = get_int("What is x: ");
+  int y = get_int("What is y: ");
+
+  if (x < y) {
+    printf("x is less than y \n");
+  } else if (x < y) {
+    printf("x is less than y \n");
+  } else {
+    printf("x is equal to y\n");
+  }
+}
