165cfde023e68133f2c91d40895336e92bebd133
Hao Nguyen <hao@haonguyen.tech>
2024-01-14 16:13:52 +0700

    feat: add cs50 library and test get_string()

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
 // Copyright (c) 2024 Hao Nguyen. All Rights Reserved.
+#include <cs50.h>
 #include <stdio.h>
 
-int main(void) { printf("hello world\n"); }
+int main(void) {
+  string answer = get_string("What is your name?: ");
+  printf("hello, %s \n", answer);
+}
