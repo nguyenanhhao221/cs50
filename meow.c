b7bc985256d22c2510a7086d9681f9b05b69c9f3
Hao Nguyen <hao@haonguyen.tech>
2024-01-14 17:05:53 +0700

    feat: add meow function

--- meow.c
+++ meow.c
@@ -1,7 +1,12 @@
 // Copyright (c) 2024 Hao Nguyen. All Rights Reserved.
 #include <stdio.h>
-int main(void) {
-  for (int i = 0; i < 3; i++) {
-    printf("meow meow \n");
+
+void meow(int n);
+
+int main(void) { meow(3); }
+
+void meow(int n) {
+  for (int i = 0; i < n; i++) {
+    printf("meow \n");
   }
 }
