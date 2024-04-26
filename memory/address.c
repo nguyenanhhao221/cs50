43dd4b96fa63887a4a1e4d3b090f3d3cf89caa41
Hao Nguyen <hao@haonguyen.tech>
2024-04-27 00:51:05 +0700

    basic without cs50 string implementation

--- memory/address.c
+++ memory/address.c
@@ -5,9 +5,13 @@ int main(void) {
 
   int *p = &n;
   // Print the memory address
-  printf("%p, n", p);
+  printf("The value of p is: %p\n", p);
 
   // From the memory address, go to where it point and show me the value
-  printf("%i, n", *p);
-  return 0;
+  printf("The value of *p is:%i\n", *p);
+
+  // Actually implementation of string in C without cs50 library
+  char *s = "HI!";
+
+  printf("The string is: %s\n", s);
 }
